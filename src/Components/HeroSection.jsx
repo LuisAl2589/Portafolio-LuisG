04164395706f130eafea851dd54fabc01b56b4fd
LuisAl2589 <61215951+LuisAl2589@users.noreply.github.com>
2024-08-27 13:06:27 -0600

    correcion cv

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HeroSection.css'; // Importa el archivo de estilos CSS
+import cv from '../assets/CV-Luis_Alfonso_Garcia_Castaneda_Esp.pdf';
 
 /**
  * HeroSection component.
@@ -15,7 +16,7 @@ const HeroSection = () => {
             <div>
                 <a href="https://www.linkedin.com/in/luis-alfonso-garcía-castañeda-7549a025a/" target='_blank'><i className="fa-brands fa-linkedin"></i></a>
 
-                <a href="..\src\assets\CV-Luis_Alfonso_Garcia_Castaneda_Esp.pdf" target='_blank'><i className="fa-solid fa-file"></i></a>
+                <a href={cv} target='_blank'><i className="fa-solid fa-file"></i></a>
                 <a href="https://github.com/LuisAl2589" target='_blank'><i className="fa-brands fa-github"></i></a>
             </div>
             <div id='correo' href="">garcia.castaneda.luis.alfonso@gmail.com</div>
